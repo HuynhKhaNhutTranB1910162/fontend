83b25ac141485fb9093eb728f8f4c71d13acee9d
tranhuynh123st <114003080+tranhuynh123st@users.noreply.github.com>
2023-03-23 17:12:46 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -12,10 +12,22 @@ const routes = [
         name: "notfound",
         component: () => import("@/views/NotFound.vue"),
     },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true
+    },
+    {
+        path: "/contacts/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+        props: true
+    },
         
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
     });
-export default router
+export default router;
